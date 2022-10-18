2136a631963698f9bb70e34a6f00fbed56ef365c
Shaily-Tripathi <tripathishaily95@gmail.com>
2022-10-18 21:57:09 +0530

    updateCart

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -2,6 +2,7 @@ const userModel = require("../models/userModel");
 const productModel = require("../models/productModel")
 const cartModel = require('../models/cartModel');
 const { isValidObjectId } = require("mongoose");
+const {isValid,  isValidRequestBody} = require("../validators/productValidation")
 
 const createCart = async function (req, res) {
     try {
@@ -73,7 +74,7 @@ const createCart = async function (req, res) {
         {
             let productExistInCart = userCart.items.findIndex(items => items.productId == requestBody.productId);
 
-            console.log(productExistInCart)
+            //console.log(productExistInCart)
 
             //if provided product does exist in cart
 
@@ -106,4 +107,94 @@ const createCart = async function (req, res) {
 
 }
 
-module.exports = { createCart }
+
+//=============================================================Update Cart==========================================================
+
+const updateCart = async function(req, res){
+    try{
+        let data = req.body
+        let userId = req.params.userId
+
+        if (!userId) return res.status(400).send({ status: false, message: "Please Provide userId in the path Params" })
+
+        if (!isValidObjectId(userId)) return res.status(400).send({ status: false, msg: "userId is not valid" })
+        
+        if(req.userId !== userId) return res.status(403).send({status:false,message:"user is not authorised"})
+
+        let checkUser = await userModel.findById(userId)
+        if (!checkUser) return res.status(404).send({ status: false, msg: "user is not found" })
+
+        if (!isValidRequestBody(data)) {
+            return res.status(400).send({ status: false, message: "request Body can't be empty" });
+        }
+
+        let {  productId, cartId, removeProduct } = data
+
+        if (!cartId) return res.status(400).send({ status: false, msg: "plz provide cartId" })
+
+        if (!isValidObjectId(cartId)) return res.status(400).send({ status: false, message: " enter a valid cartId " })
+
+        let findCart = await cartModel.findById( cartId )
+
+        if (!findCart) return res.status(404).send({ status: false, message: " cart not found" })
+
+        if (userId != findCart.userId) return res.status(403).send({ status: false, message: "Access denied, this is not your cart" })
+
+        if (!productId) return res.status(400).send({ status: false, msg: "plz provide productId" })
+
+        if (!isValidObjectId(productId)) return res.status(400).send({ status: false, message: " enter a valid productId " });
+
+        let findProduct = await productModel.findOne({ _id: productId, isDeleted: false })
+
+        if (!findProduct) return res.status(404).send({ status: false, message: " product does not exists" })
+
+        //if (!isValid(removeProduct)) return res.status(400).send({ status: false, message: " plz provide removeProduct " })
+
+              
+        if(typeof removeProduct != "number") return res.status(400).send({ status: false, message: " invalid type of removeProduct field" })
+
+        if ((removeProduct !== 0 && removeProduct !== 1)) {
+            return res.status(400).send({ status: false, message: "removeProduct value should be 0 or 1 only " })
+        }
+
+        let prices = findProduct.price
+        
+        let item = findCart.items
+
+        if(item.length == 0) return res.status(404).send({ status: false, message: "cart is empty" })
+            let flag = 0;
+            for (let i=0 ; i<item.length; i++) {
+                if (item[i].productId.toString() == productId) {
+                    flag = 1;  //set flag when given  productid is found in cart
+                    if (removeProduct == 1) {
+                        item[i].quantity--
+                        findCart.totalPrice -= prices;
+
+                    } else if(removeProduct == 0 ){
+                        let changePrice = item[i].quantity * prices
+                        findCart.totalPrice -= changePrice
+                        item[i].quantity = 0
+                    }
+                    if (item[i].quantity == 0) {
+                        item.splice(i, 1)
+                    }
+
+                }
+            }
+            if (flag == 0) { return res.status(404).send({ status: false, message: "productId not found in cart" }) } //set flag when given  productid is not found in cart
+
+
+            findCart.totalItems = item.length
+            await findCart.save()
+            let find = await cartModel.findOne({userId: userId}).populate('items.productId')
+            return res.status(200).send({ status: true, message: "Success", data: find })
+}
+
+    catch(error){
+
+        res.status(500).send({ status: false, message: error.message })
+    }
+}
+
+
+module.exports = { createCart, updateCart }
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const{createUser,loginUser, updateUser, getUserDetails} = require("../controllers/userController")
 const{createProduct, getProductDetailsById, deleteProductById, getProductDetails,productUpdate} = require("../controllers/productController")
-const{createCart}=require('../controllers/cartController')
+const{createCart, updateCart}=require('../controllers/cartController')
 const {authentication, authorization} = require('../middleware/auth')
 const router = express.Router();
 
@@ -18,8 +18,9 @@ router.get("/products/:productId",getProductDetailsById)
 router.put("/products/:productId",productUpdate)
 router.delete("/products/:productId",deleteProductById)
 
-
+//==========Feature III --> Product===================
 router.post('/users/:userId/cart',createCart)
+router.put('/users/:userId/cart',authentication,updateCart)
 
 
 
